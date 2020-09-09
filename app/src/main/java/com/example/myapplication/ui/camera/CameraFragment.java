74f270ee287c6949d22ea13117344d6c7bec6261
sfofgalaxy <3170105860@zju.edu.cn>
2020-09-09 23:31:31 +0800

    完成拍照和餐盘布局，接下来需要跳转

--- app/src/main/java/com/example/myapplication/ui/camera/CameraFragment.java
+++ app/src/main/java/com/example/myapplication/ui/camera/CameraFragment.java
@@ -1,27 +1,176 @@
 package com.example.myapplication.ui.camera;
 
 
-import android.content.Context;
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.ContentUris;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.example.myapplication.R;
-import com.example.myapplication.ui.plate.PlateViewModel;
+
+import static android.app.Activity.RESULT_OK;
 
 public class CameraFragment extends Fragment {
-
+    public static final int PICK_PHOTO = 102;
+    public static final int TAKE_CAMERA = 101;
+    private Button bt1;
+    private Button bt2;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-        Context context = getActivity().getApplicationContext();
-        PlateViewModel plateViewModel = ViewModelProviders.of(this).get(PlateViewModel.class);
         final View root = inflater.inflate(R.layout.fragment_camera, container, false);
         return root;
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        bt1 = getActivity().findViewById(R.id.bt3);
+        bt2 = getActivity().findViewById(R.id.bt4);
+        //通过摄像头拍照
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
+
+                Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);//用来打开相机的Intent
+                startActivityForResult(takePhotoIntent,TAKE_CAMERA);//启动相机
+            }
+        });
+
+        bt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
+
+                Intent openAlbumIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                openAlbumIntent.setType("image/*");
+                startActivityForResult(openAlbumIntent, PICK_PHOTO);//打开相册
+            }
+        });
+    }
+
+    @Override
+    public void onActivityResult( int requestCode, int resultCode, @Nullable Intent data){
+        switch (requestCode) {
+            case TAKE_CAMERA:
+                if (resultCode == RESULT_OK) {
+                    Bundle extras = data.getExtras();
+                    Bitmap bitmap = (Bitmap) extras.get("data");
+                    //imageView.setImageBitmap(bitmap);
+
+                    //pzfxgg在这里跳转到相机拍到的照片
+                }
+                break;
+            case PICK_PHOTO:
+                if (resultCode == RESULT_OK) {
+                    Log.i("album", "onActivityResult: ImageUriFromAlbum: "+data.getData());
+                    if (resultCode == RESULT_OK) {
+                        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.KITKAT){
+                            handleImageOnKitKat(data);//4.4之后图片解析
+                        }else {
+                            handleImageBeforeKitKat(data);//4.4之前图片解析
+                        }
+                    }
+
+
+
+
+                }
+                break;
+            default:
+                break;
+        }
+    }
+    /**
+     * 4.4版本以下对返回的图片Uri的处理：
+     * 就是从返回的Intent中取出图片Uri，直接显示就好
+     * @param data 调用系统相册之后返回的Uri
+     */
+    private void handleImageBeforeKitKat(Intent data) {
+        Uri uri = data.getData();
+        String imagePath = getImagePath(uri,null);
+        displayImage(imagePath);
+    }
+
+    /**
+     * 4.4版本以上对返回的图片Uri的处理：
+     * 返回的Uri是经过封装的，要进行处理才能得到真实路径
+     * @param data 调用系统相册之后返回的Uri
+     */
+    @TargetApi(19)
+    private void handleImageOnKitKat(Intent data) {
+        String imagePath = null;
+        Uri uri = data.getData();
+        if (DocumentsContract.isDocumentUri(getActivity(), uri)) {
+            //如果是document类型的Uri，则提供document id处理
+            String docId = DocumentsContract.getDocumentId(uri);
+            if ("com.android.providers.media.documents".equals(uri.getAuthority())) {
+                String id = docId.split(":")[1];//解析出数字格式的id
+                String selection = MediaStore.Images.Media._ID + "=" + id;
+                imagePath = getImagePath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, selection);
+            } else if ("com.android.providers.downloads.documents".equals(uri.getAuthority())) {
+                Uri contentUri = ContentUris.withAppendedId(Uri.parse("content://downloads/public_downloads"), Long.valueOf(docId));
+                imagePath = getImagePath(contentUri, null);
+            }
+        } else if ("content".equalsIgnoreCase(uri.getScheme())) {
+            //如果是content类型的uri，则进行普通处理
+            imagePath = getImagePath(uri, null);
+        } else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            //如果是file类型的uri，则直接获取路径
+            imagePath = uri.getPath();
+        }
+        displayImage(imagePath);
+    }
+
+    /**
+     * 将imagePath指定的图片显示到ImageView上
+     */
+    private void displayImage(String imagePath) {
+        if (imagePath != null) {
+            Bitmap bitmap = BitmapFactory.decodeFile(imagePath);
+            //imageView.setImageBitmap(bitmap);
+
+
+            //pzfxgg在这里跳转到相册照片的识别结果
+        } else {
+            Toast.makeText(getActivity(), "failed to get image", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * 将Uri转化为路径
+     * @param uri 要转化的Uri
+     * @param selection 4.4之后需要解析Uri，因此需要该参数
+     * @return 转化之后的路径
+     */
+    private String getImagePath(Uri uri, String selection) {
+        String path = null;
+        Cursor cursor = getActivity().getContentResolver().query(uri, null, selection, null, null);
+        if (cursor != null) {
+            if (cursor.moveToFirst()) {
+                path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+            }
+            cursor.close();
+        }
+        return path;
     }
 }
